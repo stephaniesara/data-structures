f843cd228356b323ab0d91e641775fa8780900ec
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-01-17 12:45:20 -0800

    Passes all tests for queue es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,25 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.startIndex = 0;
+    this.storage = {};
+  }
+  size () {
+    return this.count;
   }
 
+  enqueue(value) {
+    this.storage[this.count + this.startIndex] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.count > 0) {
+      this.count--;
+    }
+    var value = this.storage[this.startIndex];
+    delete this.storage[this.startIndex];
+    this.startIndex++;
+    return value;
+  }
 }
