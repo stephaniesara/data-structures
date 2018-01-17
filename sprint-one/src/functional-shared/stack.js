53587cb7f72af1206ff324155e708edc3774e19f
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-01-17 11:30:29 -0800

    Passes .size tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.count = 0;
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.count;
+};
 
+stackMethods.push = function(value) {
+  this.count++;
+};
+
+stackMethods.pop = function() {
+  if (this.count > 0) {
+    this.count--;
+  }
+};
