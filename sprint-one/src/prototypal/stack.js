c4e677bafc605b77698acf82108689cf1dfb9e6c
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-01-17 11:39:56 -0800

    Implements stack w/prototypical

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.count = 0;
+  newStack.storage = {};
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.count;
+}
 
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+}
+
+stackMethods.pop = function() {
+  if (this.count > 0) {
+    this.count--;
+  }
+  var value = this.storage[this.count];
+  delete this.storage[this.count];
+  return value;
+}
