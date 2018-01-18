94459dc04a05ef4c19b10ef4aa6cad7600509ae4
stephaniesara <stephaniesarachong@gmail.com>
2018-01-17 16:41:53 -0800

    Implements tree and most of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,38 +2,63 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = new GraphNode();
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return node in this.nodes;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  this.forEachNode(function(eachNode) {
+    if (node in eachNode.edges) {
+      delete eachNode.edges[node];
+    }
+  });
+  delete this.nodes[node];  
+  // this.nodes.forEachNode(function(eachNode) {
+  //   if (node in eachNode.edges) {
+  //     delete eachNode.edges[node];
+  //   }
+  // });
+  // delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return toNode in this.nodes[fromNode].edges;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode].edges[toNode] = true;
+  this.nodes[toNode].edges[fromNode] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.nodes[fromNode].edges[toNode];
+  delete this.nodes[toNode].edges[fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (let key in this.nodes) {
+    cb(key);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-
+var GraphNode = function() {
+  this.edges = {};
+};
