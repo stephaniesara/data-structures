3ddc400915a6bfbf7633dfb572fdf44e7d3c9ed6
stephaniesara <stephaniesarachong@gmail.com>
2018-01-18 12:38:40 -0800

    Implement BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,43 @@
 var BinarySearchTree = function(value) {
+  var newBST = Object.create(BinarySearchTree.prototype);
+  newBST.left = null;
+  newBST.right = null;
+  newBST.value = value;
+  return newBST;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+
+  var insertToSide = function(side, node) {
+    if (node[side] === null) {
+      node[side] = BinarySearchTree(value);
+    } else {
+      node[side].insert(value);
+    }
+  }
+  
+  if (value < this.value) {
+    insertToSide('left', this);
+  } else if (value > this.value) {
+    insertToSide('right', this);
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else if (this.value > value) {   
+    return !!this.left && this.left.contains(value);
+  } else {
+    return !!this.right && this.right.contains(value);
+  }  
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  !!this.left && this.left.depthFirstLog(cb);
+  !!this.right && this.right.depthFirstLog(cb);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
